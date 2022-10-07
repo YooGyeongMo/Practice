4cb3541f6bb9f21267e36755527a642b23ce2713
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-10-08 02:44:49 +0900

    자바 for문 사용 무한 루프

--- src/Homework/Q_01_4.java
+++ src/Homework/Q_01_4.java
@@ -1,4 +1,13 @@
 package Homework;
 
 public class Q_01_4 {
+    //(4) for 문을 사용하여서 무한 루프를 작성한다.
+    public static void main(String [] args){
+
+
+
+    for(;;){
+       System.out.println("시험에서 다 맞춰버릴게요 ㅎ");
+    }
+}
 }
