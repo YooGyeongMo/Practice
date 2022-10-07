4314c6f96d7617c98a20c6266c9cf8e24fda015e
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-10-08 03:27:21 +0900

    4-2번문제 풀기

---
+++ src/Homework/Q_04_2.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class Q_04_2 {
+    public static void main (String [] args) {
+        int age = 21;
+
+        if (age > 20 && age < 65)
+        {
+
+            System.out.println("1번 그룹");
+        }
+        else {
+            System.out.println("2번 그룹");
+        }
+    }//end of main
+}// end of class
