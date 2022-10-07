6bf16e4540ad1c1f2faec6038a8f412824ca9d88
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-10-08 03:15:46 +0900

    2번 문제의 코드 포함 완전한 자바프로그램으로 만들기

--- src/Homework/Q_03.java
+++ src/Homework/Q_03.java
@@ -3,9 +3,9 @@ package Homework;
 public class Q_03 {
     public static void main(String[] args) {
         //2번 문제의 코드를 포함하여서 완전한 자바 프로그램으로 작성하시오. 변수 x는 3으로 초기화한다.
-        int x = 3;
+        int x = -1;
 
-        if (x >= 0)
+        if (x > 0)
         {
             System.out.println("첫 번째 문자열");
         }
