d3ea2f132feb60fe57015bd79b5e51c336043715
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-10-08 03:03:49 +0900

    자바 정상 출력이나 문법상 맞지 않는 코드

---
+++ src/Homework/Q_02.java
@@ -0,0 +1,19 @@
+package Homework;
+
+
+
+public class Q_02 {
+    //다음 코드를 실행하였을 경우, 생성되는 출력을 쓰시오. x는 3이라고 가정한다.
+    public static void main(String[] args) {
+    int x = 3;
+       if (x >= 0) {
+            if (x == 0) {
+                System.out.println("첫 번째 문자열");
+            }
+        } else {
+            System.out.println("두 번째 문자열");
+
+            System.out.println("세 번째 문자열");
+        }
+    }
+}
