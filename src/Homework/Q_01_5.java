30774fc639e41415c520ff71fbd7e9b7c1270824
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-10-08 02:48:40 +0900

    자바 while문 사용 무한 루프

--- src/Homework/Q_01_5.java
+++ src/Homework/Q_01_5.java
@@ -1,4 +1,10 @@
 package Homework;
 
 public class Q_01_5 {
+    public static void main(String [] args){
+        //(5) while 문을 사용하여서 무한 루프를 작성한다.
+        while(true){
+            System.out.println("현재 시간 오전 3:47분! A+를 향해!!");
+        }
+    }
 }
