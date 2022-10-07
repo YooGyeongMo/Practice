5b53e9dbdfdb30f4e0e37493052cfa0194d4f8bd
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-10-08 04:03:23 +0900

    5번문제 풀기

---
+++ src/Homework/Q_05_1.java
@@ -0,0 +1,16 @@
+package Homework;
+
+public class Q_05_1 {
+    //다음 문장의 오류를 찾아서 수정하라. 오류가 없을 수도 있고 2개 이상의 오류가 있을 수도 있다. 문법적인 오류뿐만 아니라 논리적인 오류도 지적하라.
+    public static void main(String[] args){
+
+        int age = 17;
+
+        if( 0 < age && age < 18 )
+
+    {
+            System.out.println("청소년");
+    }
+    }//end of main
+
+}// end of class
---
+++ src/Homework/Q_05_2.java
@@ -0,0 +1,10 @@
+package Homework;
+
+public class Q_05_2 {
+    public static void main(String [] args){
+        int x = 0;
+
+        if( x == 0 )
+            System.out.println("x는 0이다.");
+    }
+}
