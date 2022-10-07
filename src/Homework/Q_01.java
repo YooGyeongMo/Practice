b2b7ed56ae42d6ca860a998a3ec3cd6c55054869
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-10-08 02:13:18 +0900

    자바 연습문제 풀기

--- src/Homework/Q_01.java
+++ src/Homework/Q_01.java
@@ -1,7 +1,19 @@
 package Homework;
 
+import java.util.*;
+
 public class Q_01 {
     public static void main(String [] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("정수를 입력하세요: ");
+        int count = sc.nextInt();
+        if(count>=20 && count<60) {
+            count++;
+            System.out.println("20이상 60미만인 정수입니다. count에 축적되었습니다.");
+        }
+        else{
+            System.out.println("조건에 해당하는 정수가 아닙니다.");
+            }
+        }
     }
-}
+
