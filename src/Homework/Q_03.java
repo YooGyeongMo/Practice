24c8b7c77099a37b60c6fc14f376105617fc3400
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-10-08 03:07:49 +0900

    2번 문제의 코드 포함 완전한 자바프로그램으로 만들기

---
+++ src/Homework/Q_03.java
@@ -0,0 +1,20 @@
+package Homework;
+
+public class Q_03 {
+    public static void main(String[] args) {
+        //2번 문제의 코드를 포함하여서 완전한 자바 프로그램으로 작성하시오. 변수 x는 3으로 초기화한다.
+        int x = 3;
+
+        if (x >= 0)
+        {
+            System.out.println("첫 번째 문자열");
+        }
+        else if (x == 0)
+        {
+            System.out.println("두 번째 문자열");
+        }
+        else {
+        System.out.println("세 번째 문자열");
+        }
+    } //end of main
+}// end of class
