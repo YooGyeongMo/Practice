e9c96562b584be317615078869f7ab0713c72196
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-10-08 03:16:18 +0900

    4번문제 풀기

---
+++ src/Homework/Q_04.java
@@ -0,0 +1,20 @@
+package Homework;
+
+
+
+public class Q_04 {
+    //다음 코드를 실행하였을 경우, 생성되는 출력을 쓰시오. 들여쓰기를 올바르게 수정하시오.
+    public static void main(String[] args){
+
+
+    int age = 19;
+
+    if( age > 20 ) {
+        System.out.println("1번 그룹\n");
+    }
+    else {
+        System.out.println("2번 그룹\n");
+        System.out.println("입니다.\n");
+        }
+    }//end of main
+}//end of class
