463f475f93927adda304635f1f0e949a525e7a8f
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-10-28 11:18:23 +0900

    배열

---
+++ src/Lecture/Lec_22.java
@@ -0,0 +1,9 @@
+package Lecture;
+
+public class Lec_22 {
+    public static void main(String [] args){
+        int[] scores;
+        // scores = {10,20,30,40,50};
+        scores = new int[] { 10, 20, 30, 40, 50}; // -> 무명변수
+    }
+}
