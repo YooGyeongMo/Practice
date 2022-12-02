1dc108437be6245e454ff74e547528a41f947bf0
gyeongmo Yoo <98875580+YooGyeongMo@users.noreply.github.com>
2022-12-02 10:19:50 +0900

    클래스

---
+++ src/My_Math/Math.java
@@ -0,0 +1,11 @@
+package My_Math;
+
+public class Math {
+    int square (int i){
+        return i * i;
+    }
+
+    double sqaure (double i){
+        return i * i;
+    }
+}
---
+++ src/My_Math/Math_Test.java
@@ -0,0 +1,9 @@
+package My_Math;
+
+public class Math_Test {
+    public static void main(String[] args) {
+        Math obj = new Math();
+        System.out.print(obj.sqaure(10));
+        System.out.print(obj.sqaure(3.14));
+    }
+}
